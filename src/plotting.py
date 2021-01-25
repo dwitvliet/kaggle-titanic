fdb82046a840428556e178f96e8322362e371c37
witvliet <daniel.witvliet@gmail.com>
2021-01-24 20:11:27 -0500

    Expanded docstring of learning curve plotting function

--- src/plotting.py
+++ src/plotting.py
@@ -5,7 +5,26 @@ from sklearn import model_selection
 
 def learning_curve(estimator, X, y, ax=None, cv=None, n_jobs=None,
                    train_sizes=np.linspace(.1, 1.0, 5), legend=True):
-    """ Adapted from https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+    """ Plots the test and training learning curve of an estimator.
+
+    Adapted from https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+
+    Args:
+        estimator (sklearn estimator): An estimator instance implementing `fit`
+            and `predict` methods which will be cloned for each validation.
+        X (array-like of shape (n_samples, n_features)): Training vector, where
+            `n_samples` is the number of samples and `n_features` is the number
+            of features.
+        y (array-like of shape (n_samples) or (n_samples, n_features)): Target
+            relative to ``X`` for classification or regression.
+        ax (axes object, optional): Axes to plot learning curve.
+        cv (cross-valiation generator, optional): Defaults to a `ShuffleSplit`
+            with 100 splits for a smooth curve.
+        train_sizes (array-like, optional): Points on training curve.
+        legend (boolean, optional): Whether to plot a legend.
+
+    Returns:
+        axes object
 
     """
     if ax is None:
@@ -17,6 +36,7 @@ def learning_curve(estimator, X, y, ax=None, cv=None, n_jobs=None,
     ax.set_xlabel('Training examples')
     ax.set_ylabel('Score')
 
+    # Calculate learning curve.
     train_sizes, train_scores, test_scores, fit_times, _ = model_selection.learning_curve(
         estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, return_times=True
     )
